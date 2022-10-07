9c2f71093487deb88e43323e41a2fa27aec3abe7
MireyaCR <mireya.mcr@gmail.com>
2022-10-07 11:33:17 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generatexcuse();
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("askdfjañsl");
+};
+let generatexcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoi = 0;
+  let whati = 0;
+  let wheni = 0;
+
+  return who[whoi] + " " + what[whati] + " " + when[wheni];
 };
