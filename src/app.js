35339e2e2680b1853b5892a4842fe29d24f4ecd8
MireyaCR <mireya.mcr@gmail.com>
2022-10-07 12:02:39 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -6,24 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  document.querySelector("#excuse").innerHTML = generatexcuse();
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generatexcuse();
+  });
   //write your code here
   console.log("askdfjañsl");
 };
 let generatexcuse = () => {
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
   let what = ["eat", "pissed", "crushed", "broked"];
   let when = [
-    "before the class",
-    "right in time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
+    "before the class.",
+    "right in time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
   ];
 
-  let whoi = 0;
-  let whati = 0;
-  let wheni = 0;
+  let whoi = Math.floor(Math.random() * who.length);
+  let whati = Math.floor(Math.random() * who.length);
+  let wheni = Math.floor(Math.random() * who.length);
 
   return who[whoi] + " " + what[whati] + " " + when[wheni];
 };
